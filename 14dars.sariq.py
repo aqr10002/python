430445d9e7d8a7df7600bd8295748153599c8ea0
quldosh <81145879+aqr10002@users.noreply.github.com>
2021-03-23 17:53:48 +0500

    python

---
+++ 14dars.sariq.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 15 15:33:32 2021
+
+@author: USER
+"""
+
+# 14dars   Dictionary(lug'at)
+# muallif AQR   15/03/2021...........15:37 tm
+
+#car_0 = {'model':'ferrari','rang':'qizil'}
+#print(car_0['model'])
+#print(car_0['rang'])
+
+#en_uz = {'apple':'olma','apricot':'o`rik','banana':'banan'}
+
+#mevalar = {'olma':10000,'tarvuz':5000,'shaftoli':22000}
+#print(f"Olma narhi {mevalar['olma']} so`m")
+
+#talaba_0 = {'ism':'Allayarov Quldosh','yosh':24,'t_yil':1996}
+#print(f"{talaba_0['ism'].title()},\
+#      {talaba_0['t_yil']}-yilda tug`ilgan,\
+#          {talaba_0['yosh']} yoshda")
+         
+#talaba_0['kurs'] = 4
+#talaba_0['fakultet'] = 'amaliy matematika'
+#talaba_0['ism'] = 'Ashraf'
+
+#talaba_1 ={}
+#talaba_1['ism'] = 'allayarov quldosh'
+#talaba_1['kurs'] = 1
+#talaba_1['yosh'] = 24
+#print(talaba_1)
+#talaba_1(f'talaba_0','')
+
+#name = input("ismingiz nima? ")
+#print(f"salom,  {name.title() }")
+
+#cars = ['malibu', 'lacetti','toyoto','mazda']
+#'nexia' in cars
+#'nexia' not in cars
+
+#telefonlar = {
+ #   'ali':'iphone x',
+ #   'vali':'galaxy s9',
+#    'olim':'mi 10 pro',
+  #  'odil':'nokia 3110',
+  #  'quldosh':'galacsi 9'
+  #  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
