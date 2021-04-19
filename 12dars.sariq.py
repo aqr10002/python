4a6ab9c9a5ddc8d533ea060a6c358b141dd0608c
quldosh <81145879+aqr10002@users.noreply.github.com>
2021-04-19 21:12:29 +0300

    Add files via upload

---
+++ 12dars.sariq.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 13 15:35:10 2021
+
+@author: USER
+"""
+
+# 12dars : XATOLAR BILAN ISHLASH
+# muallif: AQR 13.03.2021  SOAT 15:42
+
+#print ("Hello World!")
+#print("o`ngacha sanaymiz")
+#for n in range(10):
+#   print(n+1)
+
+# type error
+#son = input("Istalgan son kiriting:")
+#son = int(son)
+#print(f"{son} ning kvadrati {son**2} ga teng")
+
+# name error
+#mevalar = ['olma', 'uzum', 'nok', 'anor', 'anjir']
+#for meva in mevalar:
+#    print(meva)
+
+#son = float(input("Istalgan son kiriting:"))
+#if son>=0:
+#    print("musbat son")
+#else:
+#    print("manfiy son")
+
+# zerodivisionerror
+#x, y = 60, 50
+#z = 250/(x-y)
+
+# mantiqiy xatolar
+#radius = 5
+#pi = 4.14
+#aylana_yuzi = pi*radius**2
+#print(aylana_yuzi)
+
+#son = float(input("istalgan son kiriting: "))
+#ildiz = son**1/2
+#print(f"{son} ning ildizi {ildiz} ga teng")
+
+mevalar = ['olma', 'uzum', 'nok', 'anor', 'anjir']
+for meva in mevalar:
+   print(meva)
+   
+print("Dastur tugadi")
+
+
+#print("Dastur tugadi")
+
+#savol="son kiriting<<"
+#    if x%2==0:
+#        print("juft son kiritilgan")
+#        break    else:
+#        print("Juft son kiriting")
+
+
+#mevalar = ['olma', 'anor', 'uzum']
+#print(mevalar [3])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
